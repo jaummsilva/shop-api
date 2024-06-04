5dc52a2d482fe4c05af1d7e4d863e71dd75d29b3
João Vitor da Silva <150824087+sz-joao-silva@users.noreply.github.com>
2024-06-04 08:19:40 -0300

    Feat: profile image added

--- src/infra/http/controllers/profile/presenter/profile-presenter.ts
+++ src/infra/http/controllers/profile/presenter/profile-presenter.ts
@@ -5,11 +5,13 @@ export class ProfilePresenter {
     name: string
     email: string
     role: 'ADMIN' | 'MEMBER'
+    imageUrl: string
   } {
     return {
       name: user.name,
       email: user.email,
       role: user.role,
+      imageUrl: `/user/${user.id.toString()}/get-image/` + user.imageFakeName,
     }
   }
 }
